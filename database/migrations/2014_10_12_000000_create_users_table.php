095c2bc93ca50083baab5a5b82b0b27f7b9d51b1
Alexander <alexander.hidalgoc@gmail.com>
2019-06-13 17:10:00 -0300

    user admin puede ser nulo

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name', 100);
-            $table->boolean('is_admin',0-1);
+            $table->boolean('is_admin',0-1)->nullable();
             $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
