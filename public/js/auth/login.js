caf942198574931a53a291a09ce73747f21dfd8e
DigitalHouse <you@example.com>
2019-07-11 12:10:32 -0300

    clase 1107

--- public/js/auth/login.js
+++ public/js/auth/login.js
@@ -4,49 +4,49 @@ window.onload = function () {
   var password = document.querySelector('#password');
   var formulario = document.querySelector('form');
 
-  // email.onfocus = function () {
-  //     this.classList.remove('is-invalid');
-  // }
-  // email.onblur = function () {
-  //   if (this.value.trim() == '') {
-  //     email.classList.add('is-invalid');
-  //     var error = document.createElement('span');
-  //     error.setAttribute('class','invalid-feedback');
-  //     error.innerText = 'El campo EMAIL no puede estar vacio!';
-  //     var div = email.parentElement;
-  //     if (div.children[2]) {
-  //         div.removeChild(div.children[2]);
-  //     }
-  //     div.append(error);
-  //   } else if (!regexEmail.test(this.value)) {
-  //     email.classList.add('is-invalid');
-  //     var error = document.createElement('span');
-  //     error.setAttribute('class','invalid-feedback');
-  //     error.innerText = 'El campo EMAIL no tiene un formato valido!';
-  //     var div = email.parentElement;
-  //     if (div.children[2]) {
-  //         div.removeChild(div.children[2]);
-  //     }
-  //     div.append(error);
-  //   }
-  // }
-  //
-  // password.onfocus = function () {
-  //     this.classList.remove('is-invalid');
-  // }
-  // password.onblur = function () {
-  //   if (this.value.trim() == '') {
-  //     password.classList.add('is-invalid');
-  //     var error = document.createElement('span');
-  //     error.setAttribute('class','invalid-feedback');
-  //     error.innerText = 'El campo PASSWORD no puede estar vacio!';
-  //     var div = password.parentElement;
-  //     if (div.children[2]) {
-  //         div.removeChild(div.children[2]);
-  //     }
-  //     div.append(error);
-  //   }
-  // }
+  email.onfocus = function () {
+      this.classList.remove('is-invalid');
+  }
+  email.onblur = function () {
+    if (this.value.trim() == '') {
+      email.classList.add('is-invalid');
+      var error = document.createElement('span');
+      error.setAttribute('class','invalid-feedback');
+      error.innerText = 'El campo EMAIL no puede estar vacio!';
+      var div = email.parentElement;
+      if (div.children[2]) {
+          div.removeChild(div.children[2]);
+      }
+      div.append(error);
+    } else if (!regexEmail.test(this.value)) {
+      email.classList.add('is-invalid');
+      var error = document.createElement('span');
+      error.setAttribute('class','invalid-feedback');
+      error.innerText = 'El campo EMAIL no tiene un formato valido!';
+      var div = email.parentElement;
+      if (div.children[2]) {
+          div.removeChild(div.children[2]);
+      }
+      div.append(error);
+    }
+  }
+
+  password.onfocus = function () {
+      this.classList.remove('is-invalid');
+  }
+  password.onblur = function () {
+    if (this.value.trim() == '') {
+      password.classList.add('is-invalid');
+      var error = document.createElement('span');
+      error.setAttribute('class','invalid-feedback');
+      error.innerText = 'El campo PASSWORD no puede estar vacio!';
+      var div = password.parentElement;
+      if (div.children[2]) {
+          div.removeChild(div.children[2]);
+      }
+      div.append(error);
+    }
+  }
 
   formulario.onsubmit = function (event) {
     if (email.value.trim() == '') {
--- public/js/auth/register.js
+++ public/js/auth/register.js
@@ -24,89 +24,89 @@ function pintarError(input, mensaje) {
 
 window.onload = function () {
     var form = document.querySelector('form');
-    // var email = document.querySelector('#email');
-    // var name = document.querySelector('#name');
-    // var password = document.querySelector('#password');
-    // var passwordConfirm = document.querySelector('#password-confirm');
-    //
-    //
-    // email.onfocus = function () {
-    //     this.classList.remove('is-invalid');
-    // }
-    // email.onblur = function () {
-    //   if (this.value.trim() == '') {
-    //     email.classList.add('is-invalid');
-    //     var error = document.createElement('span');
-    //     error.setAttribute('class','invalid-feedback');
-    //     error.innerText = 'El campo EMAIL no puede estar vacio!';
-    //     var div = email.parentElement;
-    //     if (div.children[2]) {
-    //         div.removeChild(div.children[2]);
-    //     }
-    //     div.append(error);
-    //   } else if (!regexEmail.test(this.value)) {
-    //     email.classList.add('is-invalid');
-    //     var error = document.createElement('span');
-    //     error.setAttribute('class','invalid-feedback');
-    //     error.innerText = 'El campo EMAIL no tiene un formato valido!';
-    //     var div = email.parentElement;
-    //     if (div.children[2]) {
-    //         div.removeChild(div.children[2]);
-    //     }
-    //     div.append(error);
-    //   }
-    // }
-    //
-    // name.onfocus = function () {
-    //     this.classList.remove('is-invalid');
-    // }
-    // name.onblur = function () {
-    //   if (this.value.trim() == '') {
-    //     name.classList.add('is-invalid');
-    //     var error = document.createElement('span');
-    //     error.setAttribute('class','invalid-feedback');
-    //     error.innerText = 'El campo NOMBRE no puede estar vacio!';
-    //     var div = name.parentElement;
-    //     if (div.children[2]) {
-    //         div.removeChild(div.children[2]);
-    //     }
-    //     div.append(error);
-    //   }
-    // }
-    //
-    // password.onfocus = function () {
-    //     this.classList.remove('is-invalid');
-    // }
-    // password.onblur = function () {
-    //   if (this.value.trim() == '') {
-    //     password.classList.add('is-invalid');
-    //     var error = document.createElement('span');
-    //     error.setAttribute('class','invalid-feedback');
-    //     error.innerText = 'El campo PASSWORD no puede estar vacio!';
-    //     var div = password.parentElement;
-    //     if (div.children[2]) {
-    //         div.removeChild(div.children[2]);
-    //     }
-    //     div.append(error);
-    //   }
-    // }
-    //
-    // passwordConfirm.onfocus = function () {
-    //     this.classList.remove('is-invalid');
-    // }
-    // passwordConfirm.onblur = function () {
-    //   if (this.value.trim() == '') {
-    //     passwordConfirm.classList.add('is-invalid');
-    //     var error = document.createElement('span');
-    //     error.setAttribute('class','invalid-feedback');
-    //     error.innerText = 'El campo CONFIRMAR PASSWORD no puede estar vacio!';
-    //     var div = passwordConfirm.parentElement;
-    //     if (div.children[2]) {
-    //         div.removeChild(div.children[2]);
-    //     }
-    //     div.append(error);
-    //   }
-    // }
+    var email = document.querySelector('#email');
+    var name = document.querySelector('#name');
+    var password = document.querySelector('#password');
+    var passwordConfirm = document.querySelector('#password-confirm');
+
+
+    email.onfocus = function () {
+        this.classList.remove('is-invalid');
+    }
+    email.onblur = function () {
+      if (this.value.trim() == '') {
+        email.classList.add('is-invalid');
+        var error = document.createElement('span');
+        error.setAttribute('class','invalid-feedback');
+        error.innerText = 'El campo EMAIL no puede estar vacio!';
+        var div = email.parentElement;
+        if (div.children[2]) {
+            div.removeChild(div.children[2]);
+        }
+        div.append(error);
+      } else if (!regexEmail.test(this.value)) {
+        email.classList.add('is-invalid');
+        var error = document.createElement('span');
+        error.setAttribute('class','invalid-feedback');
+        error.innerText = 'El campo EMAIL no tiene un formato valido!';
+        var div = email.parentElement;
+        if (div.children[2]) {
+            div.removeChild(div.children[2]);
+        }
+        div.append(error);
+      }
+    }
+
+    name.onfocus = function () {
+        this.classList.remove('is-invalid');
+    }
+    name.onblur = function () {
+      if (this.value.trim() == '') {
+        name.classList.add('is-invalid');
+        var error = document.createElement('span');
+        error.setAttribute('class','invalid-feedback');
+        error.innerText = 'El campo NOMBRE no puede estar vacio!';
+        var div = name.parentElement;
+        if (div.children[2]) {
+            div.removeChild(div.children[2]);
+        }
+        div.append(error);
+      }
+    }
+
+    password.onfocus = function () {
+        this.classList.remove('is-invalid');
+    }
+    password.onblur = function () {
+      if (this.value.trim() == '') {
+        password.classList.add('is-invalid');
+        var error = document.createElement('span');
+        error.setAttribute('class','invalid-feedback');
+        error.innerText = 'El campo PASSWORD no puede estar vacio!';
+        var div = password.parentElement;
+        if (div.children[2]) {
+            div.removeChild(div.children[2]);
+        }
+        div.append(error);
+      }
+    }
+
+    passwordConfirm.onfocus = function () {
+        this.classList.remove('is-invalid');
+    }
+    passwordConfirm.onblur = function () {
+      if (this.value.trim() == '') {
+        passwordConfirm.classList.add('is-invalid');
+        var error = document.createElement('span');
+        error.setAttribute('class','invalid-feedback');
+        error.innerText = 'El campo CONFIRMAR PASSWORD no puede estar vacio!';
+        var div = passwordConfirm.parentElement;
+        if (div.children[2]) {
+            div.removeChild(div.children[2]);
+        }
+        div.append(error);
+      }
+    }
 
 
     form.onsubmit = function (event) {
